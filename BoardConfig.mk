6e2b89410c1d0785b5e95d2ce44edb5f3efdf8b0
Mesquita <mesquita@aospa.co>
2021-07-09 13:28:31 -0300

    [DNM] umi: Bring back display configs
     * As FoD devices are unable to boot using qti display component
       when using prebuilt hwcomposer and displayfeature
    
     * We only have 3 devices with FoD so move these configs for
       device specific tree
    
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,13 @@ BUILD_BROKEN_DUP_RULES := true
 
 # Display
 TARGET_SCREEN_DENSITY := 440
+TARGET_USES_COLOR_METADATA := true
+TARGET_USES_DISPLAY_RENDER_INTENTS := true
+TARGET_USES_DRM_PP := true
+TARGET_USES_GRALLOC1 := true
+TARGET_USES_GRALLOC4 := true
+TARGET_USES_HWC2 := true
+TARGET_USES_ION := true
 
 # Fingerprint
 SOONG_CONFIG_XIAOMI_KONA_FOD_POS_X = 439
@@ -33,6 +40,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/vintf/xiaomi_vendor
 
 # Kernel
 KERNEL_DEFCONFIG := umi_defconfig
+
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 
 # inherit from the proprietary version
 include vendor/xiaomi/umi/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -35,6 +35,70 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
+    libqdMetaData \
+    libsdmcore \
+    libsdmutils \
+    libtinyxml \
+    libvulkan \
+    memtrack.kona \
+    vendor.display.config@1.5 \
+    vendor.display.config@1.11.vendor \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.gralloc.gfx_ubwc_disable=0 \
+    debug.sf.enable_hwc_vds=1 \
+    debug.sf.latch_unsignaled=1 \
+    persist.vendor.color.matrix=2 \
+    vendor.display.comp_mask=0 \
+    vendor.display.disable_excl_rect=0 \
+    vendor.display.disable_excl_rect_partial_fb=1 \
+    vendor.display.disable_hw_recovery_dump=1 \
+    vendor.display.disable_offline_rotator=1 \
+    vendor.display.disable_scaler=0 \
+    vendor.display.enable_optimize_refresh=1 \
+    vendor.display.enable_posted_start_dyn=1 \
+    vendor.display.use_smooth_motion=1 \
+    debug.egl.hw=0 \
+    debug.mdpcomp.logs=0 \
+    debug.sf.enable_advanced_sf_phase_offset=1 \
+    debug.sf.enable_gl_backpressure=1 \
+    debug.sf.high_fps_early_gl_phase_offset_ns=-4000000 \
+    debug.sf.high_fps_early_phase_offset_ns=-4000000 \
+    debug.sf.high_fps_late_app_phase_offset_ns=1000000 \
+    debug.sf.high_fps_late_sf_phase_offset_ns=-4000000 \
+    debug.sf.hw=0 \
+    debug.sf.latch_unsignaled=1 \
+    persist.demo.hdmirotationlock=false \
+    persist.sys.sf.color_mode=0 \
+    persist.sys.sf.color_saturation=1.0 \
+    persist.sys.sf.native_mode=0
+
+# Fingerprint
+TARGET_HAS_FOD := true
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.kona
+
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
